6b90ecfb5de525470ee778551117be40339bcd21
shrub <shrub719@gmail.com>
2025-03-06 11:56:00 +0000

    added basic api endpoint

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+app.get("/api", (req, res) => {
+    res.json({ message: "hi from the server!!" });
+});
+
+
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
 });
