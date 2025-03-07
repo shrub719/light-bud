84d602456eea1076955de238e52f55d2bb1e5c7c
shrub <shrub719@gmail.com>
2025-03-07 20:36:22 +0000

    added permissions

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -3,6 +3,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+    manifest: {
+        permissions: [
+            "storage",
+            "notifications",
+            "tabs",
+            "alarms"
+        ]
+    },
     srcDir: 'src',
     extensionApi: 'chrome',
     modules: ['@wxt-dev/module-react'],
