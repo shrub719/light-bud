47fb522605f27b80ff6bd933f0907c27a3fd0db3
shrub <shrub719@gmail.com>
2025-03-07 19:25:38 +0000

    added tailwind.css

--- extension/src/entrypoints/popup/App.tsx
+++ extension/src/entrypoints/popup/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
 import './App.css';
 
 function App() {
@@ -10,10 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
+          <img src="" className="logo" alt="WXT logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>WXT + React</h1>
--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     srcDir: 'src',
     extensionApi: 'chrome',
     modules: ['@wxt-dev/module-react'],
+    vite: () => ({
+        plugins: [tailwindcss()],
+    }),
 });
