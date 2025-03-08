7f795e5065ba32d6b88449916936b606c168ae32
shrub <shrub719@gmail.com>
2025-03-08 19:01:45 +0000

    added todos

--- extension/src/entrypoints/background.ts
+++ extension/src/entrypoints/background.ts
@@ -2,5 +2,7 @@ export default defineBackground({
     persistent: true,
     main() {
         console.log("hello from background.ts!", { id: browser.runtime.id });
+        // TODO: when intialising extension, what if server isn't online/available?
+        // TODO: dynamically load customisation options from server? so new hats don't need an extension update.
     },
 });
