35829e5d4d7a25f3807d27bffc5b7cdcb9ef71b5
shrub <shrub719@gmail.com>
2025-03-09 11:40:21 +0000

    added shop field to User schema

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     uuid: { type: String, required: true, unique: true },
     auth: {
         key: { type: String, required: true },
-        salt: { type: String, required: true }
+        salt: { type: String, required: true },
     },
     stats: {
         focusHours: { type: Number, default: 0 },
@@ -12,6 +12,9 @@ const userSchema = new mongoose.Schema({
     profile: {
         username: { type: String, default: "Light Bud User" },
         icon: { type: [String], default: ["cat-happy", "blue"] } 
+    },
+    shop: {
+        unlocked: { type: [String], default: [] }
     }
 });
 
