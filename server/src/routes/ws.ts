52d0d928c0a91e2b312c6d484d1279c6f211f64d
shrub <shrub719@gmail.com>
2025-03-15 12:33:08 +0000

    added disconnect listener

--- server/src/routes/ws.ts
+++ server/src/routes/ws.ts
@@ -3,5 +3,9 @@ import { Server } from "socket.io";
 export default function setupWebSocket(ioInstance: Server) {
     ioInstance.on("connection", (socket) => {
         console.log("user connected");
+
+        socket.on("disconnect", () => {
+            console.log("user disconnected");
+        });
     });
 }
--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -3,6 +3,12 @@ import { User, Room } from "../utils/models"
 import { Document, ObjectId } from "mongodb";
 import { Request, Response } from "express";
 import * as auth from "../utils/auth";
+import { RegExpMatcher, englishDataset, englishRecommendedTransformers } from "obscenity";
+
+const matcher = new RegExpMatcher({
+    ...englishDataset.build(),
+    ...englishRecommendedTransformers,
+});
 
 export default async function connectDB() {
     try {
@@ -29,13 +35,32 @@ export async function registerUser(req: Request) {
 }
 
 // TODO: require credentials for websocket
-export async function getUser(uuid: string, isUser=false) {
+export async function getUser(uuid: string, isUser=false): Promise<[Document, Object]> {
     const user = await User.findById({ _id: uuid });
-    return [user, auth.stripAuth(user as Document, !isUser)];
+    return [user as Document, auth.stripAuth(user as Document, !isUser)];
 }
 
-export async function editUser(uuid: string, edits: object) {
+// TODO: keep user document as a variable in a websocket?
+export async function editUser(uuid: string, edits: any) {
     const [user, strippedUser] = await getUser(uuid, true);
+
+    if (edits.stats) user.stats = edits.stats;
+    if (edits.profile) {
+        const username = edits.profile.username;
+        if (matcher.hasMatch(username)) {
+            return res.status(400).json({ error: "user-badlanguage" });
+        }
+        if (!(1 <= username.length && username.length <= 20)) {
+            return res.status(400).json({ error: "user-length" });
+        }
+        if (!auth.validateUsername(username)) {
+            return res.status(400).json({ error: "user-special" });
+        }
+        user.profile = edits.profile;
+    }
+
+    const savedUser = await user.save();
+    return auth.stripAuth(savedUser);
 }
 
 
