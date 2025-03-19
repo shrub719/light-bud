8c7e97649c5a8e092c473276287c1d8ce3000f3e
shrub <shrub719@gmail.com>
2025-03-19 18:43:28 +0000

    updated register function

--- extension/src/entrypoints/background/index.ts
+++ extension/src/entrypoints/background/index.ts
@@ -1,23 +1,18 @@
-import { initUser } from "./utils/user";
+import { register } from "./utils/user";
 
 export default defineBackground({
     persistent: true,
     main() {
-        // TODO: nothing stopping users from editing localStorage shop.unlocked
-        //       NEVERMIND: remember to double-check shop data from every user edit
-
         // first install
         // TODO: what if server isn't online/available?
         // TODO: remember to change urls from localhost
         browser.runtime.onInstalled.addListener(async ({ reason }: any) => {
             if (reason === "install") {
                 console.log("extension installed");
-                const ok = await initUser();
+                const ok = await register();
             }
         });
 
         console.log("hello from background.ts!", { id: browser.runtime.id });
-        // TODO: dynamically load customisation options from server? so new hats don't need an extension update.
-        // TODO: remember to sanitise data just in case
     },
 });
--- extension/src/entrypoints/background/utils/user.ts
+++ extension/src/entrypoints/background/utils/user.ts
@@ -1,5 +1,5 @@
-export async function initUser(): Promise<boolean> {
-    const response = await fetch("http://localhost:3002/api/users/create", {
+export async function register(): Promise<boolean> {
+    const response = await fetch("http://localhost:3002/api/register", {
         method: "POST"
     });
 
@@ -8,17 +8,17 @@ export async function initUser(): Promise<boolean> {
     const json = await response.json();
     console.log(json);
     await browser.storage.local.set({
-        uuid: json.user.uuid,
+        uuid: json.user._id,
         key: json.unhashedKey,
         stats: json.user.stats,
         profile: json.user.profile,
+        room: json.user.room,
         shop: json.user.shop
     });
     console.log("user initialised");
     return true;
 }
 
-// TODO: is this a good idea? to reset it every time it's passed to a server?
 // TODO: make a typescript type for the user object?
 // wrapper function that every response containing the user's own data is passed through
 export async function verifyShop(json: any): Promise<void> {
