c6cf917ebf7db70c254f9fd812f3a8431975314a
shrub <shrub719@gmail.com>
2025-03-19 18:49:41 +0000

    added pet interface

---
+++ extension/src/entrypoints/background/utils/pet.ts
@@ -0,0 +1,6 @@
+// TODO: have one global pet interface that you extend for background and popup?
+//       the other one could have animator attached to a state
+interface Pet {
+    health: number,
+    name: string
+}
