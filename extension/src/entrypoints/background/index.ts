103749054f587991a13a0fc129e801a39016818e
shrub <shrub719@gmail.com>
2025-03-23 12:53:26 +0000

    added basic message receiving

--- extension/src/entrypoints/background/index.ts
+++ extension/src/entrypoints/background/index.ts
@@ -4,14 +4,21 @@ import { onMessage } from "webext-bridge/background";
 export default defineBackground({
     persistent: true,
     main() {
-        let user: u.User | null;
+        let user: u.User;
+
         // first install
         browser.runtime.onInstalled.addListener(async ({ reason }: any) => {
             if (reason === "install") {
                 console.log("extension installed");
                 let ok;
-                [user, ok] = await u.register();
+                [user, ok] = await u.register() as [u.User, boolean];  // FIX
             }
+        });
+
+        // messages
+        onMessage("set-profile", ({ profile }: any) => {  // FIX
+            user.profile = profile;
+            u.save(user);
         });
 
         console.log("hello from background.ts!", { id: browser.runtime.id });
